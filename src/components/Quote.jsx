15ebb7151e890632da2f4bcb58c8674c44cca76b
Barshon <barshonweb@gmail.com>
2024-10-06 05:09:06 +0600

    quote (responsive) and animation

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import "./quote.css";
 import quote from "../assets/Icon/quotes.svg";
 import pat from "../assets/img/img (5).png";
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
+AOS.init();
 const Quote = () => {
   return (
     <div className="quote flex flex-col md:flex-row items-center justify-center px-5 md:justify-between md:px-12 bg-gradient-to-r  from-red-600 to-red-700 text-white font-bold text-md md:text-2xl  ">
@@ -9,13 +12,13 @@ const Quote = () => {
         <div>
           <img src={quote} className="w-1/6 " alt="" />
         </div>
-        <div className="flex flex-col mx-5 tracking-wider relative -top-8 ">
+        <div data-aos="fade-in" data-aos-easing="ease-in-out" className="flex flex-col mx-5 tracking-wider relative -top-8 ">
           <p className="uppercase">Our mission is to enlighten, entertain</p>
           <p className="uppercase">and empower current and future </p>
           <p className="uppercase">leaders around the world. </p>
         </div>
       </div>
-      <div className="lg:w-1/4 md:w-1/3 relative -top-14 flex justify-center items-center md:right-8 order-1 md:order-2  ">
+      <div data-aos="fade-left" className="lg:w-1/4 md:w-1/3 relative -top-14 flex justify-center items-center md:right-8 order-1 md:order-2  ">
         <img src={pat} alt="" />
       </div>
     </div>
