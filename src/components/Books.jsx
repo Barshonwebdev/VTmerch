2848c15248eb7a8c16b1dc237ca968f6ad15d91a
Barshon <barshonweb@gmail.com>
2024-10-06 02:37:47 +0600

    book collection animation

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -13,15 +13,15 @@ const Books = () => {
             <p className="text-3xl book-font bg-gradient-to-b from-red-600 to-red-500 text-transparent bg-clip-text font-bold">SIGNED</p>
             <p className="text-3xl book-font bg-gradient-to-b from-red-600 to-red-500 text-transparent bg-clip-text font-bold">COLLECTION</p>
             <div>
-                <button className="btn mt-10 px-8 bg-white text-black rounded-xl py-0 book-font">SHOP NOW <FaArrowRight></FaArrowRight> </button>
+                <button className="transition ease-in delay-100 hover:scale-110 btn mt-10 px-8 bg-white text-black rounded-xl py-0 book-font">SHOP NOW <FaArrowRight></FaArrowRight> </button>
             </div>
             <div className="">
                 <img className="  mt-8" src={signature} alt="" />
             </div>
         </div>
         <div className="flex flex-col md:flex-row md:justify-end">
-            <img className=" md:w-52 lg:w-64" src={book1} alt="" />
-            <img className="relative left-5 md:left-0 md:w-1/2 lg:w-1/3" src={book2} alt="" />
+            <img className="transition ease-in delay-100 hover:scale-110 md:w-52 lg:w-64" src={book1} alt="" />
+            <img className="transition ease-in delay-100 hover:scale-110 relative left-5 md:left-0 md:w-1/2 lg:w-1/3" src={book2} alt="" />
         </div>
       </div>
     </div>
