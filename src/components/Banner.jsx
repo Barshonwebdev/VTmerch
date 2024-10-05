65ebd40ee39eb59f060529f25edcf78c2f70144a
Barshon <barshonweb@gmail.com>
2024-10-05 18:57:50 +0600

    banner products responsive with slider animation

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import "swiper/css/autoplay";
+
+
 import "./banner.css";
 import logo from "../assets/Logo/Valuetainment.svg";
 import bannerproduct1 from '../assets/img/img (9).png'
@@ -7,11 +13,11 @@ import bannerproduct3 from '../assets/img/product3.jpeg'
 import bannerproduct4 from '../assets/img/product2.png'
 const Banner = () => {
   return (
-    <div className=" text-white px-12 pt-5 ">
+    <div className=" text-white px-5 md:px-12 pt-5  ">
       <div className="flex justify-between ">
-        <div className="flex space-x-3">
+        <div className="flex items-center space-x-3">
         <img className="w-10 logo " src={logo} alt="" />
-        <h1 className="text-6xl font-bold ">VTMERCH</h1>
+        <h1 className=" text-2xl md:text-6xl font-bold ">VTMERCH</h1>
         </div>
         <div>
           <button className="btn btn-square btn-ghost">
@@ -32,13 +38,88 @@ const Banner = () => {
         </div>
       </div>
       {/* slogan  */}
-      <div className="mt-16 ms-8 flex space-x-12">
-        <h1 className="slogan text-6xl opacity-50">FUTURE </h1>
-        <h1 className="slogan text-6xl opacity-50">LOOKS </h1>
-        <h1 className="slogan text-6xl opacity-50">BRIGHTS!</h1>
+      <div className="md:mt-16 mt-7 ms-3 md:ms-8 flex space-x-6 md:space-x-12">
+        <h1 className="slogan text-2xl md:text-6xl opacity-50">FUTURE </h1>
+        <h1 className="slogan text-2xl md:text-6xl opacity-50">LOOKS </h1>
+        <h1 className="slogan text-2xl md:text-6xl opacity-50">BRIGHTS!</h1>
       </div>
-      {/* banner products  */}
-      <div className="pb-10 ms-12 relative -top-5 flex space-x-4">
+      {/* banner products large screen */}
+     
+     <div className="pb-10 ms-10 hidden  relative md:-top-5 -top-3 flex-col md:flex-row justify-center items-center md:flex md:space-x-2 space-y-5 md:space-y-0">
+     <Swiper  modules={[Autoplay]} slidesPerView={5} spaceBetween={10} autoplay={{
+          delay: 1500,
+          disableOnInteraction: true,
+        }} >
+        <SwiperSlide><div>
+            <img src={bannerproduct1} className="w-56 rounded-lg" alt="" />
+            <p className="text-xs my-3 ">Army Green Active Short Sleeve</p>
+            <div className="flex space-x-2">
+              <p className="text-xs">$ 34.95</p>
+              <p className="text-xs line-through">$ 34.95</p>
+            </div>
+        </div></SwiperSlide>
+        <SwiperSlide><div>
+            <img src={bannerproduct2} className="w-56 rounded-lg" alt="" />
+            <p className="text-xs my-3 ">Army Green Active Short Sleeve</p>
+            <div className="flex space-x-2">
+              <p className="text-xs">$ 34.95</p>
+              <p className="text-xs line-through">$ 34.95</p>
+            </div>
+        </div></SwiperSlide>
+        <SwiperSlide><div>
+            <img src={bannerproduct3} className="w-56 rounded-lg" alt="" />
+            <p className="text-xs my-3 ">Army Green Active Short Sleeve</p>
+            <div className="flex space-x-2">
+              <p className="text-xs">$ 34.95</p>
+              <p className="text-xs line-through">$ 34.95</p>
+            </div>
+        </div></SwiperSlide>
+       <SwiperSlide> <div>
+            <img src={bannerproduct4} className="w-56 rounded-lg" alt="" />
+            <p className="text-xs my-3 ">Army Green Active Short Sleeve</p>
+            <div className="flex space-x-2">
+              <p className="text-xs">$ 34.95</p>
+              <p className="text-xs line-through">$ 34.95</p>
+            </div>
+        </div></SwiperSlide>
+       <SwiperSlide> <div>
+            <img src={bannerproduct3} className="w-56 rounded-lg" alt="" />
+            <p className="text-xs my-3 ">Army Green Active Short Sleeve</p>
+            <div className="flex space-x-2">
+              <p className="text-xs">$ 34.95</p>
+              <p className="text-xs line-through">$ 34.95</p>
+            </div>
+        </div></SwiperSlide>
+       <SwiperSlide> <div>
+            <img src={bannerproduct4} className="w-56 rounded-lg" alt="" />
+            <p className="text-xs my-3 ">Army Green Active Short Sleeve</p>
+            <div className="flex space-x-2">
+              <p className="text-xs">$ 34.95</p>
+              <p className="text-xs line-through">$ 34.95</p>
+            </div>
+        </div></SwiperSlide>
+        <SwiperSlide><div>
+            <img src={bannerproduct1} className="w-56 rounded-lg" alt="" />
+            <p className="text-xs my-3 ">Army Green Active Short Sleeve</p>
+            <div className="flex space-x-2">
+              <p className="text-xs">$ 34.95</p>
+              <p className="text-xs line-through">$ 34.95</p>
+            </div>
+        </div></SwiperSlide>
+       <SwiperSlide> <div>
+            <img src={bannerproduct2} className="w-56 rounded-lg" alt="" />
+            <p className="text-xs my-3 ">Army Green Active Short Sleeve</p>
+            <div className="flex space-x-2">
+              <p className="text-xs">$ 34.95</p>
+              <p className="text-xs line-through">$ 34.95</p>
+            </div>
+        </div></SwiperSlide>
+        </Swiper>
+      </div>
+      {/* banner products small screen */}
+     
+     <div className="pb-10 md:hidden  relative md:-top-5 -top-3 flex-col md:flex-row justify-center items-center flex md:space-x-2 space-y-5 md:space-y-0">
+     
         <div>
             <img src={bannerproduct1} className="w-56 rounded-lg" alt="" />
             <p className="text-xs my-3 ">Army Green Active Short Sleeve</p>
@@ -79,7 +160,34 @@ const Banner = () => {
               <p className="text-xs line-through">$ 34.95</p>
             </div>
         </div>
+        <div>
+            <img src={bannerproduct4} className="w-56 rounded-lg" alt="" />
+            <p className="text-xs my-3 ">Army Green Active Short Sleeve</p>
+            <div className="flex space-x-2">
+              <p className="text-xs">$ 34.95</p>
+              <p className="text-xs line-through">$ 34.95</p>
+            </div>
+        </div>
+        <div>
+            <img src={bannerproduct1} className="w-56 rounded-lg" alt="" />
+            <p className="text-xs my-3 ">Army Green Active Short Sleeve</p>
+            <div className="flex space-x-2">
+              <p className="text-xs">$ 34.95</p>
+              <p className="text-xs line-through">$ 34.95</p>
+            </div>
+        </div>
+        <div>
+            <img src={bannerproduct2} className="w-56 rounded-lg" alt="" />
+            <p className="text-xs my-3 ">Army Green Active Short Sleeve</p>
+            <div className="flex space-x-2">
+              <p className="text-xs">$ 34.95</p>
+              <p className="text-xs line-through">$ 34.95</p>
+            </div>
+        </div>
+        
       </div>
+     
+      
     </div>
   );
 };
