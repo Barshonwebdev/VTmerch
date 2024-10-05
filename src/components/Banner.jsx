85cb0a129c9d0daa2fae9339e1363462d7059855
Barshon <barshonweb@gmail.com>
2024-10-05 13:34:36 +0600

    banner products

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import "./banner.css";
 import logo from "../assets/Logo/Valuetainment.svg";
+import bannerproduct1 from '../assets/img/img (9).png'
+import bannerproduct2 from '../assets/img/product1.jpg'
+import bannerproduct3 from '../assets/img/product3.jpeg'
+import bannerproduct4 from '../assets/img/product2.png'
 const Banner = () => {
   return (
-    <div className=" text-white px-7 pt-5 ">
+    <div className=" text-white px-12 pt-5 ">
       <div className="flex justify-between ">
         <div className="flex space-x-3">
         <img className="w-10 logo " src={logo} alt="" />
@@ -27,10 +31,54 @@ const Banner = () => {
           </button>
         </div>
       </div>
+      {/* slogan  */}
       <div className="mt-16 ms-8 flex space-x-12">
         <h1 className="slogan text-6xl opacity-50">FUTURE </h1>
-        <h1 className="slogan text-6xl opacity-50"> LOOKS </h1>
+        <h1 className="slogan text-6xl opacity-50">LOOKS </h1>
         <h1 className="slogan text-6xl opacity-50">BRIGHTS!</h1>
+      </div>
+      {/* banner products  */}
+      <div className="pb-10 ms-12 relative -top-5 flex space-x-4">
+        <div>
+            <img src={bannerproduct1} className="w-56 rounded-lg" alt="" />
+            <p className="text-xs my-3 ">Army Green Active Short Sleeve</p>
+            <div className="flex space-x-2">
+              <p className="text-xs">$ 34.95</p>
+              <p className="text-xs line-through">$ 34.95</p>
+            </div>
+        </div>
+        <div>
+            <img src={bannerproduct2} className="w-56 rounded-lg" alt="" />
+            <p className="text-xs my-3 ">Army Green Active Short Sleeve</p>
+            <div className="flex space-x-2">
+              <p className="text-xs">$ 34.95</p>
+              <p className="text-xs line-through">$ 34.95</p>
+            </div>
+        </div>
+        <div>
+            <img src={bannerproduct3} className="w-56 rounded-lg" alt="" />
+            <p className="text-xs my-3 ">Army Green Active Short Sleeve</p>
+            <div className="flex space-x-2">
+              <p className="text-xs">$ 34.95</p>
+              <p className="text-xs line-through">$ 34.95</p>
+            </div>
+        </div>
+        <div>
+            <img src={bannerproduct4} className="w-56 rounded-lg" alt="" />
+            <p className="text-xs my-3 ">Army Green Active Short Sleeve</p>
+            <div className="flex space-x-2">
+              <p className="text-xs">$ 34.95</p>
+              <p className="text-xs line-through">$ 34.95</p>
+            </div>
+        </div>
+        <div>
+            <img src={bannerproduct3} className="w-56 rounded-lg" alt="" />
+            <p className="text-xs my-3 ">Army Green Active Short Sleeve</p>
+            <div className="flex space-x-2">
+              <p className="text-xs">$ 34.95</p>
+              <p className="text-xs line-through">$ 34.95</p>
+            </div>
+        </div>
       </div>
     </div>
   );
